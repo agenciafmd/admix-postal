226fdbbec8e765864a921143f92b6fa23b5ac030
Carlos Tamagawa <carlos@mixd.com.br>
2025-04-22 15:35:39 -0300

    fix(menu) ajusta o visible

--- src/Http/Components/Aside/Postal.php
+++ src/Http/Components/Aside/Postal.php
@@ -4,7 +4,8 @@ namespace Agenciafmd\Postal\Http\Components\Aside;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
-
+use Illuminate\Support\Facades\Gate;
+use Agenciafmd\Postal\Models\Postal as ModelPostal;
 class Postal extends Component
 {
     public function __construct(
@@ -21,7 +22,7 @@ class Postal extends Component
         $this->label = __(config('admix-postal.name'));
         $this->url = route('admix.postal.index');
         $this->active = request()?->currentRouteNameStartsWith('admix.postal');
-        $this->visible = true;
+        $this->visible = Gate::allows('view', ModelPostal::class);
 
         return view('admix::components.aside.item');
     }
